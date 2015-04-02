88d11ef235ef42d86a1008f3393ad61199505be9
David Howden <dhowden@gmail.com>
2015-04-03 00:23:59 +1100

    Improve tchstore command comment

--- cmd/tchstore/main.go
+++ cmd/tchstore/main.go
@@ -3,26 +3,25 @@
 // license that can be found in the LICENSE file.
 
 /*
-tchstore is a tool to create a remote store for tchaik files, including index, media and artwork files.
+tchstore is a tool to create a remote store for tchaik files, including media and artwork files (TODO: index).
 
 It is assumed that tchstore is run relatively local to the data it is serving (i.e. in EC2 for S3, or on a
 fileserver).
 
-All configuration is done through command line parameters.
+All configuration is done through command line parameters, use --help flag for full details.
 
 To fetch files from the `tchaik-store` S3 bucket (and store media fetches in local cache under `media-cache`
 directory, and artwork in a content addressable cache under the directory `artwork-cache`):
 
   tchstore -listen 0.0.0.0:1844 -root s3://tchaik-store/ -artwork-cache artwork-cache -media-cache media-cache
 
-By default the artwork store will try to:
+Implementation note: by default the artwork store will try to:
   1) retrieve files from the local artwork cache (if the cache exists)
   2) retrieve data from media files in a local store (media-cache or root if local)
   3) fetch the whole media file from S3 (if configured), which will in turn add the
      media file to (media-cache).
 
-If the artwork cache is enabled, then all fetched artwork will be cached on the first access.
-
+Set the environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY to pass credentials to the S3 client.
 */
 package main
 
