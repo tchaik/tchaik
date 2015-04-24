680cda67279768435018c91de8417ff0c9efe1ec
Chris Trotman <chris@trotman.io>
2015-04-25 09:14:03 +1000

    [gulp] Implement sass compilation

---
+++ cmd/tchaik/ui/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+    sass: {
+        src: ['static/sass/**/*.scss'],
+        dest: 'static/css'
+    }
+};
+
+gulp.task('sass', function() {
+    return gulp.src(paths.sass.src)
+               .pipe(sourcemaps.init())
+               .pipe(sass())
+               .pipe(sourcemaps.write(
+                   paths.sass.dest,
+                   {sourceRoot: '/static/sass'}
+               ))
+               .pipe(gulp.dest(paths.sass.dest));
+});
+
+gulp.task('default', []);
