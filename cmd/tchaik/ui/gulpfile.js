74b4b974a45eeb04e058a76c93ca96bb396d42f8
Chris Trotman <chris@trotman.io>
2015-04-25 10:11:39 +1000

    [gulp] Compile the browserify bundle with gulp

--- cmd/tchaik/ui/gulpfile.js
+++ cmd/tchaik/ui/gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
+var _ = require('lodash');
 
+var browserify = require('browserify');
+var envify = require('envify')
+var gutil = require('gulp-util');
+var reactify = require('reactify');
 var sass = require('gulp-sass');
+var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
+var watchify = require('watchify');
 
 var paths = {
     sass: {
         src: ['static/sass/**/*.scss'],
         dest: 'static/css'
+    },
+    js: {
+        entry: './static/js/src/app.js',
+        bundleName: 'tchaik.js',
+        dest: 'static/js/build'
     }
 };
 
@@ -21,8 +33,41 @@ gulp.task('sass', function() {
                .pipe(gulp.dest(paths.sass.dest));
 });
 
+function bundle(watch) {
+    var bundler, rebundle;
+
+    bundler = browserify(paths.js.entry, {
+        debug: true,
+        cache: {},
+        packageCache: {},
+        fullPaths: !watch
+    });
+
+    if (watch) {
+        bundler = watchify(bundler);
+    }
+
+    bundler.transform(reactify);
+    bundler.transform(envify);
+
+    rebundle = function() {
+        var stream = bundler.bundle();
+        stream.on('error', gutil.log.bind(gutil, 'Browserify Error'));
+        stream = stream.pipe(source(paths.js.bundleName));
+        return stream.pipe(gulp.dest(paths.js.dest));
+    };
+
+    bundler.on('update', rebundle);
+    return rebundle();
+}
+
+gulp.task('js', function() {
+    return bundle(false);
+});
+
 gulp.task('watch', function() {
     gulp.watch(paths.sass.src, ['sass']);
+    bundle(true);
 });
 
 gulp.task('default', []);
