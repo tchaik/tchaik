891814f9381f879c235a3207c3c7ab929f6a3206
David Howden <dhowden@gmail.com>
2015-04-25 13:47:30 +1000

    Made indentation of gulpfile.js 2 spaces

--- cmd/tchaik/ui/gulpfile.js
+++ cmd/tchaik/ui/gulpfile.js
@@ -16,126 +16,126 @@ var sourcemaps = require('gulp-sourcemaps');
 var watchify = require('watchify');
 
 var paths = {
-    sass: {
-        src: ['static/sass/**/*.scss'],
-        dest: 'static/css'
-    },
-    js: {
-        entry: './static/js/src/app.js',
-        bundleName: 'tchaik.js',
-        dest: 'static/js/build'
-    },
-    jshint: {
-        src: [
-            'package.json',
-            'gulpfile.js',
-            'static/js/src/app.js',
-            'static/js/src/actions/*.js',
-            'static/js/src/stores/*.js',
-            'static/js/src/constants/*.js',
-            'static/js/src/utils/*.js'
-        ]
-    }
+  sass: {
+    src: ['static/sass/**/*.scss'],
+    dest: 'static/css'
+  },
+  js: {
+    entry: './static/js/src/app.js',
+    bundleName: 'tchaik.js',
+    dest: 'static/js/build'
+  },
+  jshint: {
+    src: [
+      'package.json',
+      'gulpfile.js',
+      'static/js/src/app.js',
+      'static/js/src/actions/*.js',
+      'static/js/src/stores/*.js',
+      'static/js/src/constants/*.js',
+      'static/js/src/utils/*.js'
+    ]
+  }
 };
 
 gulp.task('sass', function() {
-    return gulp.src(paths.sass.src)
-               .pipe(sourcemaps.init())
-               .pipe(sass())
-               .pipe(sourcemaps.write(
-                   paths.sass.dest,
-                   {sourceRoot: '/static/sass'}
-               ))
-               .pipe(gulp.dest(paths.sass.dest));
+  return gulp.src(paths.sass.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write(
+      paths.sass.dest,
+      {sourceRoot: '/static/sass'}
+    ))
+    .pipe(gulp.dest(paths.sass.dest));
 });
 
 function bundle(watch) {
-    var bundler, rebundle;
+  var bundler, rebundle;
 
-    bundler = browserify(paths.js.entry, {
-        debug: true,
-        cache: {},
-        packageCache: {},
-        fullPaths: !watch
-    });
+  bundler = browserify(paths.js.entry, {
+    debug: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: !watch
+  });
 
-    if (watch) {
-        bundler = watchify(bundler);
+  if (watch) {
+    bundler = watchify(bundler);
+  }
+
+  bundler.transform(reactify);
+  bundler.transform(envify);
+
+  rebundle = function(changedFiles) {
+    var compileStream = bundler.bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source(paths.js.bundleName))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(sourcemaps.write(
+        './',
+        {sourceRoot: '/'}
+      ))
+      .pipe(gulp.dest(paths.js.dest))
+      .pipe(notify({message: function() { gutil.log("Built JS"); }, onLast: true}));
+
+    if (changedFiles) {
+      var lintStream = gulp.src(changedFiles)
+        .pipe(react())
+        .pipe(jshint(jshintConfig))
+        .pipe(jshint.reporter('jshint-stylish'));
+      return merge(lintStream, compileStream);
     }
+    return compileStream;
+  };
 
-    bundler.transform(reactify);
-    bundler.transform(envify);
-
-    rebundle = function(changedFiles) {
-        var compileStream = bundler.bundle()
-              .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-              .pipe(source(paths.js.bundleName))
-              .pipe(buffer())
-              .pipe(sourcemaps.init({loadMaps: true}))
-              .pipe(sourcemaps.write(
-                  './',
-                  {sourceRoot: '/'}
-              ))
-              .pipe(gulp.dest(paths.js.dest))
-              .pipe(notify({message: function() { gutil.log("Built JS"); }, onLast: true}));
-
-        if (changedFiles) {
-            var lintStream = gulp.src(changedFiles)
-            .pipe(react())
-            .pipe(jshint(jshintConfig))
-            .pipe(jshint.reporter('jshint-stylish'));
-            return merge(lintStream, compileStream);
-        }
-        return compileStream;
-    };
-
-    bundler.on('update', rebundle);
-    return rebundle();
+  bundler.on('update', rebundle);
+  return rebundle();
 }
 
 gulp.task('js', function() {
-    return bundle(false);
+  return bundle(false);
 });
 
 var jshintConfig = {
-    esnext: true,
-    browser: true,
-    devel: true,
-    curly: true,
-    undef: true,
-    unused: true,
-    node: true,
-    newcap: false,
+  esnext: true,
+  browser: true,
+  devel: true,
+  curly: true,
+  undef: true,
+  unused: true,
+  node: true,
+  newcap: false,
 };
 
 gulp.task('jshint', function() {
-    gulp.src(paths.jshint.src)
-        .on('error',gutil.log.bind(gutil, 'JSHint Error'))
-        .pipe(jshint(jshintConfig))
-        .pipe(jshint.reporter('jshint-stylish'));
+  gulp.src(paths.jshint.src)
+    .on('error',gutil.log.bind(gutil, 'JSHint Error'))
+    .pipe(jshint(jshintConfig))
+    .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('jshint:jsx', function() {
-    return gulp.src(['static/js/src/components/*.js'])
-               .pipe(jshint({
-                   linter: require('jshint-jsx').JSXHINT,
-                   esnext: true,
-                   browser: true,
-                   devel: true,
-                   jquery: true,
-                   curly: true,
-                   undef: true,
-                   unused: true,
-                   node: true,
-                   newcap: false
-               }))
-               .pipe(jshint.reporter('jshint-stylish'))
-               .pipe(jshint.reporter('fail'));
+  return gulp.src(['static/js/src/components/*.js'])
+    .pipe(jshint({
+      linter: require('jshint-jsx').JSXHINT,
+      esnext: true,
+      browser: true,
+      devel: true,
+      jquery: true,
+      curly: true,
+      undef: true,
+      unused: true,
+      node: true,
+      newcap: false
+    }))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(paths.sass.src, ['sass']);
-    bundle(true);
+  gulp.watch(paths.sass.src, ['sass']);
+  bundle(true);
 });
 
 gulp.task('default', ['sass', 'js', 'jshint:jsx']);
