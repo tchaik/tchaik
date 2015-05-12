0df7d7f8a7adcd7e68e66c8c8246ede2ffd461c4
David Howden <dhowden@gmail.com>
2015-05-12 23:27:21 +1000

    Moved Volume control component to separate file

--- cmd/tchaik/ui/static/js/src/components/LeftColumn.js
+++ cmd/tchaik/ui/static/js/src/components/LeftColumn.js
@@ -14,12 +14,10 @@ import Covers from './Covers.js';
 import Filter from './Filter.js';
 import Recent from './Recent.js';
 import Settings from './Settings.js';
+import Volume from './Volume.js';
 
 import LeftColumnStore from '../stores/LeftColumnStore.js';
 import LeftColumnActions from '../actions/LeftColumnActions.js';
-
-import VolumeStore from '../stores/VolumeStore.js';
-import VolumeActions from '../actions/VolumeActions.js';
 
 
 function getToolBarItemState(mode) {
@@ -135,92 +133,3 @@ export default class LeftColumn extends React.Component {
     this.setState(leftColumnState());
   }
 }
-
-
-function _getOffsetTop(elem) {
-  var offsetTop = 0;
-  do {
-    if (!isNaN(elem.offsetTop)) {
-        offsetTop += elem.offsetTop;
-    }
-  } while ((elem = elem.offsetParent));
-  return offsetTop;
-}
-
-function getVolumeStatus() {
-  return {
-    volume: VolumeStore.getVolume(),
-  };
-}
-
-class Volume extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = getVolumeStatus();
-    this._onChange = this._onChange.bind(this);
-    this._onWheel = this._onWheel.bind(this);
-  }
-
-  componentDidMount() {
-    VolumeStore.addChangeListener(this._onChange);
-  }
-
-  componentWillUnmount() {
-    VolumeStore.removeChangeListener(this._onChange);
-  }
-
-  render() {
-    var volume = this.state.volume;
-    var classSuffix;
-    if (volume === 0.00) {
-      classSuffix = 'off';
-    } else if (volume < 0.5) {
-      classSuffix = 'down';
-    } else {
-      classSuffix = 'up';
-    }
-
-    var h = this.props.height - parseInt(volume * this.props.height);
-    return (
-      <div className="volume" onWheel={this._onWheel} >
-        <div className="bar" onClick={this._onClick} style={{height: this.props.height}}>
-          <div className="current" style={{height: h}} />
-          <div className="marker" style={{height: this.props.markerHeight}} />
-        </div>
-        <Icon icon={'volume-' + classSuffix} onClick={this._toggleMute} />
-      </div>
-    );
-  }
-
-  _toggleMute(evt) {
-    evt.stopPropagation();
-    VolumeActions.toggleVolumeMute();
-  }
-
-  _onWheel(evt) {
-    evt.stopPropagation();
-    var v = this.state.volume + 0.05 * evt.deltaY;
-    if (v > 1.0) {
-      v = 1.0;
-    } else if (v < 0.00) {
-      v = 0.0;
-    }
-    VolumeActions.volume(v);
-  }
-
-  _onClick(evt) {
-    var pos = evt.pageY - _getOffsetTop(evt.currentTarget);
-    var height = evt.currentTarget.offsetHeight;
-    VolumeActions.volume(1 - pos/height);
-  }
-
-  _onChange() {
-    this.setState(getVolumeStatus());
-  }
-}
-
-Volume.propTypes = {
-  height: React.PropTypes.number.isRequired,
-  markerHeight: React.PropTypes.number.isRequired,
-};
