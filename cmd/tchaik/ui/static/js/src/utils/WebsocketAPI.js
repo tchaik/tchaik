8cd11b2cd7b7c4d2b70557e0d8c4de1a8475037d
David Howden <dhowden@gmail.com>
2015-05-13 23:06:39 +1000

    [es6] Consolidated WebsocketAPI code into one class

--- cmd/tchaik/ui/static/js/src/utils/WebsocketAPI.js
+++ cmd/tchaik/ui/static/js/src/utils/WebsocketAPI.js
@@ -9,51 +9,40 @@ import AppDispatcher from '../dispatcher/AppDispatcher.js';
 import WebsocketActions from '../actions/WebsocketActions.js';
 import WebsocketConstants from '../constants/WebsocketConstants.js';
 
-var _host = null;
-
-var _websocket = {
-  open: false,
-  queue: [],
-  sock: null
-};
-
-function init(host) {
-  if (_host === null) {
-    _host = host;
-  }
-
-  try {
-    _websocket.sock = new WebSocket(host);
-  } catch (exception) {
-    console.log("Error created websocket");
-    console.log(exception);
-    return;
-  }
-
-  _websocket.sock.onmessage = onMessage;
-  _websocket.sock.onerror = onError;
-  _websocket.sock.onopen = onOpen;
-  _websocket.sock.onclose = onClose;
-}
-
-function onMessage(obj) {
-  var msg = JSON.parse(obj.data);
-  WebsocketActions.dispatch(msg);
-}
-
-function onError(err) {
-  console.error(err);
-}
 
 class WebsocketAPI extends Store {
+  constructor() {
+    super();
+
+    this.host = null;
+    this.open = false;
+    this.queue = [];
+    this.sock = null;
+
+    this.dispatchToken = AppDispatcher.register(this._handleViewAction.bind(this));
+  }
+
   init(host) {
-    init(host);
+    if (this.host === null) {
+      this.host = host;
+    }
+
+    try {
+      this.sock = new WebSocket(host);
+    } catch (exception) {
+      console.log("Error created websocket");
+      console.log(exception);
+      return;
+    }
+
+    this.sock.onmessage = this._onMessage.bind(this);
+    this.sock.onerror = this._onError.bind(this);
+    this.sock.onopen = this._onOpen.bind(this);
+    this.sock.onclose = this._onClose.bind(this);
   }
 
   getStatus() {
-    return {
-      'open': _websocket.open
-    };
+    return {'open': this.open};
   }
 
   send(action, data) {
@@ -61,44 +50,49 @@ class WebsocketAPI extends Store {
       action: action,
       data: data
     };
-    if (!_websocket.open) {
-      _websocket.queue.push(payload);
+    if (!this.open) {
+      this.queue.push(payload);
       return;
     }
-    _websocket.sock.send(JSON.stringify(payload));
+    this.sock.send(JSON.stringify(payload));
   }
-}
 
-var _websocketAPI = new WebsocketAPI();
+  _handleViewAction(payload) {
+    var action = payload.action;
+    var source = payload.source;
 
-function onOpen() {
-  _websocket.open = true;
-  _websocketAPI.emitChange();
-  _websocket.queue.map(function(payload) {
-    _websocketAPI.send(payload.action, payload.data);
-  });
-  _websocket.queue = [];
-}
-
-
-function onClose() {
-  _websocket.open = false;
-  _websocketAPI.emitChange();
-}
-
-_websocketAPI.dispatchToken = AppDispatcher.register(function(payload) {
-  var action = payload.action;
-  var source = payload.source;
-
-  if (source === 'VIEW_ACTION') {
-    switch (action.actionType) {
-      case WebsocketConstants.RECONNECT:
-        init(_host);
-        break;
+    if (source === 'VIEW_ACTION') {
+      switch (action.actionType) {
+        case WebsocketConstants.RECONNECT:
+          init(_host);
+          break;
+      }
     }
+    return true;
   }
 
-  return true;
-});
+  _onMessage(obj) {
+    var msg = JSON.parse(obj.data);
+    WebsocketActions.dispatch(msg);
+  }
 
-export default _websocketAPI;
+  _onError(err) {
+    console.error(err);
+  }
+
+  _onOpen() {
+    this.open = true;
+    this.emitChange();
+    this.queue.map(function(payload) {
+      this.send(payload.action, payload.data);
+    }.bind(this));
+    this.queue = [];
+  }
+
+  _onClose() {
+    this.open = false;
+    this.emitChange();
+  }
+}
+
+export default new WebsocketAPI();
