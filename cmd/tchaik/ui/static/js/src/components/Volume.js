413598eb37639510b352b04300fbdae709a0c7f0
David Howden <dhowden@gmail.com>
2015-05-16 23:38:08 +1000

    Add fade animation to volume control hiding

--- cmd/tchaik/ui/static/js/src/components/Volume.js
+++ cmd/tchaik/ui/static/js/src/components/Volume.js
@@ -18,16 +18,18 @@ function _getOffsetTop(elem) {
   return offsetTop;
 }
 
+function getVolumeState() {
+  return {volume: VolumeStore.getVolume()};
+}
+
 
 export default class Volume extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {barVisible: false, volume: VolumeStore.getVolume()};
+    this.state = getVolumeState();
     this._onChange = this._onChange.bind(this);
     this._onWheel = this._onWheel.bind(this);
-    this._onMouseEnter = this._onMouseEnter.bind(this);
-    this._onMouseLeave = this._onMouseLeave.bind(this);
   }
 
   componentDidMount() {
@@ -49,20 +51,13 @@ export default class Volume extends React.Component {
       classSuffix = 'up';
     }
 
-    var bar = null;
-    if (this.state.barVisible) {
-      var h = this.props.height - parseInt(volume * this.props.height);
-      bar = (
+    var h = this.props.height - parseInt(volume * this.props.height);
+    return (
+      <div className="volume" onWheel={this._onWheel}>
         <div className="bar" onClick={this._onClick} style={{height: this.props.height}}>
           <div className="current" style={{height: h}} />
           <div className="marker" style={{height: this.props.markerHeight}} />
         </div>
-      );
-    }
-
-    return (
-      <div className="volume" onWheel={this._onWheel} onMouseEnter={this._onMouseEnter} onMouseLeave={this._onMouseLeave}>
-        {bar}
         <Icon icon={'volume-' + classSuffix} onClick={this._toggleMute} />
       </div>
     );
@@ -91,15 +86,7 @@ export default class Volume extends React.Component {
   }
 
   _onChange() {
-    this.setState({volume: VolumeStore.getVolume()});
-  }
-
-  _onMouseEnter() {
-    this.setState({barVisible: true});
-  }
-
-  _onMouseLeave() {
-    this.setState({barVisible: false});
+    this.setState(getVolumeState());
   }
 }
 
