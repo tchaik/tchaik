b441c611099497f9e9cde99daa653f081be81aa2
David Howden <dhowden@gmail.com>
2015-06-23 19:58:33 +1000

    Update logging filesystems to use log.Logger.
    Also fix some missing godoc comments.

--- store/log.go
+++ store/log.go
@@ -8,52 +8,71 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 // LogFileSystem returns a wrapper around an http.FileSystem which logs calls
 // to Open.
 func LogFileSystem(prefix string, fs http.FileSystem) http.FileSystem {
+	if prefix != "" {
+		prefix += " "
+	}
 	return logFileSystem{
 		FileSystem: fs,
-		prefix:     prefix,
+		Logger:     log.New(os.Stderr, prefix, log.LstdFlags),
 	}
 }
 
 type logFileSystem struct {
 	http.FileSystem
-	prefix string
+	*log.Logger
 }
 
+// Open implements http.FileSystem.
 func (l logFileSystem) Open(path string) (http.File, error) {
 	f, err := l.FileSystem.Open(path)
 	if err != nil {
-		log.Printf("%v open error: %v (%v)", l.prefix, path, err)
+		l.Printf("open error: %v", err)
 		return nil, err
 	}
-	log.Printf("%v open: %v", l.prefix, path)
+	l.Printf("open: %v", path)
 	return f, err
 }
 
 // LogRWFileSystem returns a wrapper around a RWFileSystem which logs calls
 // to Open and Create.
 func LogRWFileSystem(prefix string, fs RWFileSystem) RWFileSystem {
+	if prefix != "" {
+		prefix += " "
+	}
 	return logRWFileSystem{
 		RWFileSystem: fs,
-		prefix:       prefix,
+		Logger:       log.New(os.Stderr, prefix, log.LstdFlags),
 	}
 }
 
 type logRWFileSystem struct {
 	RWFileSystem
-	prefix string
+	*log.Logger
 }
 
+// Open implements RWFileSystem.
 func (l logRWFileSystem) Open(path string) (http.File, error) {
-	log.Printf("%v open: %v", l.prefix, path)
-	return l.RWFileSystem.Open(path)
+	f, err := l.RWFileSystem.Open(path)
+	if err != nil {
+		l.Printf("open error: %v", err)
+		return nil, err
+	}
+	l.Printf("open: %v", path)
+	return f, err
 }
 
+// Create implements RWFileSystem.
 func (l logRWFileSystem) Create(path string) (io.WriteCloser, error) {
-	log.Printf("%v create: %v", l.prefix, path)
-	return l.RWFileSystem.Create(path)
+	f, err := l.RWFileSystem.Create(path)
+	if err != nil {
+		l.Printf("create error: %v", err)
+	}
+	l.Printf("create: %v", path)
+	return f, err
 }
