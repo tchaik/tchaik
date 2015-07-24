c7f1407ceed73a413a109350565aeac336ea200a
David Howden <dhowden@gmail.com>
2015-07-24 23:22:18 +1000

    Add license comment to attr.go.

--- index/attr/attr.go
+++ index/attr/attr.go
@@ -1,3 +1,7 @@
+// Copyright 2015, David Howden
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package attr
 
 // Getter is an interface which is implemented by types which want to export typed
