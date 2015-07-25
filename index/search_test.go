e6e0b782e4be1977bccde8c79229ccc29c3e0ac4
David Howden <dhowden@gmail.com>
2015-07-25 11:55:30 +1000

    Added tests for OrderedIntersection.

--- index/search_test.go
+++ index/search_test.go
@@ -6,6 +6,7 @@ package index
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -150,6 +151,70 @@ func TestWordIndex(t *testing.T) {
 			if !reflect.DeepEqual(ps, v) {
 				t.Errorf("[%d] does't match: %#v, %#v", ii, ps, v)
 			}
+		}
+	}
+}
+
+func stringToPath(s string) Path {
+	return stringSliceToPath(strings.Split(s, ":"))
+}
+
+func stringSliceToPath(s []string) Path {
+	p := make(Path, len(s))
+	for i, x := range s {
+		p[i] = Key(x)
+	}
+	return p
+}
+
+func TestOrderedIntersection(t *testing.T) {
+	tests := []struct {
+		in  [][]Path
+		out []Path
+	}{
+		{
+			in:  nil,
+			out: []Path{},
+		},
+
+		{
+			in: [][]Path{
+				{stringToPath("A")},
+			},
+			out: []Path{stringToPath("A")},
+		},
+
+		{
+			in: [][]Path{
+				{stringToPath("A")},
+				{stringToPath("B")},
+			},
+			out: []Path{},
+		},
+
+		{
+			in: [][]Path{
+				{stringToPath("A")},
+				{stringToPath("B"), stringToPath("A")},
+			},
+			out: []Path{stringToPath("A")},
+		},
+
+		{
+			in: [][]Path{
+				{stringToPath("A"), stringToPath("B")},
+				{stringToPath("B"), stringToPath("A")},
+				{stringToPath("A"), stringToPath("B"), stringToPath("C")},
+				{stringToPath("C"), stringToPath("A"), stringToPath("B"), stringToPath("B")},
+			},
+			out: []Path{stringToPath("B"), stringToPath("A")},
+		},
+	}
+
+	for ii, tt := range tests {
+		got := OrderedIntersection(tt.in...)
+		if !reflect.DeepEqual(got, tt.out) {
+			t.Errorf("[%d] got %#v, expected: %#v", ii, got, tt.out)
 		}
 	}
 }
