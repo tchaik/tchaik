d95b60b518a9b908de22ef132187fe8bd91743bf
David Howden <dhowden@gmail.com>
2015-07-27 22:04:27 +1000

    Remove old godoc comment from group.go.

--- index/group.go
+++ index/group.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package group defines the concept of a group which is an ordered list of
-// playable items
 package index
 
 import (
