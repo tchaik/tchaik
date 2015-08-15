64ce1b683d7616095b2762e985751b5d03567785
David Howden <dhowden@gmail.com>
2015-08-15 11:09:03 +0100

    Added godoc to favourite Filter

--- index/favourite/filter.go
+++ index/favourite/filter.go
@@ -2,10 +2,13 @@ package favourite
 
 import "tchaik.com/index"
 
+// Filter is a wrapper for Store which defines the Filter method.
 type Filter struct {
 	Store
 }
 
+// Filter combs through the list of paths, returning paths which are favourites (as
+// determined by the underlying Store).
 func (f *Filter) Filter(paths []index.Path) []index.Path {
 	result := make([]index.Path, 0, len(paths))
 	for _, p := range paths {
