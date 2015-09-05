536d8ac06d2fcdf6e361a3be5dd4d168f19c8274
David Howden <dhowden@gmail.com>
2015-09-05 21:03:06 +0100

    Refactored AudioAPI into ES6 class.

--- cmd/tchaik/ui/js/src/utils/AudioAPI.js
+++ cmd/tchaik/ui/js/src/utils/AudioAPI.js
@@ -8,166 +8,169 @@ var PlayingStatusStore = require("../stores/PlayingStatusStore.js");
 
 var VolumeStore = require("../stores/VolumeStore.js");
 
-var _audio = new Audio();
-var _src = null;
-var _playing = false;
 
-function buffered() {
-  return _audio.buffered;
-}
+class AudioAPI {
+  constructor() {
+    this._audio = new Audio();
+    this._src = null;
+    this._playing = false;
 
-function play() {
-  _audio.play();
-}
+    this.onPlayerEvent = this.onPlayerEvent.bind(this);
 
-function pause() {
-  _audio.pause();
-}
+    this._audio.addEventListener("error", this.onPlayerEvent);
+    this._audio.addEventListener("progress", this.onPlayerEvent);
+    this._audio.addEventListener("play", this.onPlayerEvent);
+    this._audio.addEventListener("pause", this.onPlayerEvent);
+    this._audio.addEventListener("ended", this.onPlayerEvent);
+    this._audio.addEventListener("timeupdate", this.onPlayerEvent);
+    this._audio.addEventListener("loadedmetadata", this.onPlayerEvent);
+    this._audio.addEventListener("loadstart", this.onPlayerEvent);
 
-function load() {
-  _audio.load();
-}
+    this.update = this.update.bind(this);
+    this._onNowPlayingControl = this._onNowPlayingControl.bind(this);
+    this._onVolumeChange = this._onVolumeChange.bind(this);
 
-function src() {
-  return _src;
-}
+    NowPlayingStore.addChangeListener(this.update);
+    NowPlayingStore.addControlListener(this._onNowPlayingControl);
+    VolumeStore.addChangeListener(this._onVolumeChange);
+  }
 
-function setSrc(l) {
-  _audio.src = l;
-  _src = l;
-}
+  init() {
+    this.update();
+    this._onVolumeChange();
+  }
 
-function setCurrentTime(t) {
-  _audio.currentTime = t;
-}
+  buffered() {
+    return this._audio.buffered;
+  }
 
-function currentTime() {
-  return _audio.currentTime;
-}
+  play() {
+    return this._audio.play();
+  }
 
-function setVolume(t) {
-  _audio.volume = t;
-}
+  pause() {
+    return this._audio.pause();
+  }
 
-function volume() {
-  return _audio.volume;
-}
+  load() {
+    return this._audio.load();
+  }
 
-function duration() {
-  return _audio.duration;
-}
+  src() {
+    return this._src;
+  }
 
-function onPlayerEvent(evt) {
-  switch (evt.type) {
-  case "error":
-    NowPlayingActions.setError(evt.srcElement.error);
-    console.log("Error received from Audio component:");
-    console.error(evt);
-    break;
+  setSrc(src) {
+    this._audio.src = src;
+    this._src = src;
+  }
 
-  case "progress":
-    var range = buffered();
-    if (range.length > 0) {
-      NowPlayingActions.setBuffered(range.end(range.length - 1));
+  setCurrentTime(t) {
+    this._audio.currentTime = t;
+  }
+
+  currentTime() {
+    return this._audio.currentTime;
+  }
+
+  setVolume(v) {
+    this._audio.volume = v;
+  }
+
+  volume() {
+    return this._audio.volume;
+  }
+
+  duration() {
+    return this._audio.duration;
+  }
+
+  onPlayerEvent(evt) {
+    switch (evt.type) {
+    case "error":
+      NowPlayingActions.setError(evt.srcElement.error);
+      console.log("Error received from Audio component:");
+      console.error(evt);
+      break;
+
+    case "progress":
+      var range = this.buffered();
+      if (range.length > 0) {
+        NowPlayingActions.setBuffered(range.end(range.length - 1));
+      }
+      break;
+
+    case "play":
+      NowPlayingActions.playing(true);
+      break;
+
+    case "pause":
+      NowPlayingActions.playing(false);
+      break;
+
+    case "ended":
+      NowPlayingActions.ended(NowPlayingStore.getSource(), NowPlayingStore.getRepeat());
+      break;
+
+    case "timeupdate":
+      NowPlayingActions.currentTime(this.currentTime());
+      break;
+
+    case "loadedmetadata":
+      NowPlayingActions.setDuration(this.duration());
+
+      this.setCurrentTime(PlayingStatusStore.getTime());
+      if (this._playing) {
+        this.play();
+      }
+      break;
+
+    case "loadstart":
+      NowPlayingActions.reset();
+      break;
+
+    default:
+      console.warn("unhandled player event:");
+      console.warn(evt);
+      break;
     }
-    break;
-
-  case "play":
-    NowPlayingActions.playing(true);
-    break;
-
-  case "pause":
-    NowPlayingActions.playing(false);
-    break;
-
-  case "ended":
-    NowPlayingActions.ended(NowPlayingStore.getSource(), NowPlayingStore.getRepeat());
-    break;
-
-  case "timeupdate":
-    NowPlayingActions.currentTime(currentTime());
-    break;
-
-  case "loadedmetadata":
-    NowPlayingActions.setDuration(duration());
-
-    setCurrentTime(PlayingStatusStore.getTime());
-    if (_playing) {
-      play();
-    }
-    break;
-
-  case "loadstart":
-    NowPlayingActions.reset();
-    break;
-
-  default:
-    console.warn("unhandled player event:");
-    console.warn(evt);
-    break;
   }
-}
 
-function _onNowPlayingControl(type, value) {
-  if (type === NowPlayingConstants.SET_CURRENT_TIME) {
-    setCurrentTime(value);
-  }
-}
-
-function update() {
-  var track = NowPlayingStore.getTrack();
-  if (track) {
-    var source = `/track/${track.ID}`;
-    var orig = src();
-    if (orig !== source) {
-      setSrc(source);
-      load();
-      play();
+  _onNowPlayingControl(type, value) {
+    if (type === NowPlayingConstants.SET_CURRENT_TIME) {
+      this.setCurrentTime(value);
     }
   }
 
-  var prevPlaying = _playing;
-  _playing = NowPlayingStore.getPlaying();
-  if (prevPlaying !== _playing) {
-    if (_playing) {
-      play();
-    } else {
-      pause();
+  update() {
+    var track = NowPlayingStore.getTrack();
+    if (track) {
+      var source = `/track/${track.ID}`;
+      var orig = this.src();
+      if (orig !== source) {
+        this.setSrc(source);
+        this.load();
+        this.play();
+      }
+    }
+
+    var prevPlaying = this._playing;
+    this._playing = NowPlayingStore.getPlaying();
+    if (prevPlaying !== this._playing) {
+      if (this._playing) {
+        this.play();
+      } else {
+        this.pause();
+      }
+    }
+  }
+
+  _onVolumeChange() {
+    var v = VolumeStore.getVolume();
+    if (this.volume() !== v) {
+      this.setVolume(v);
     }
   }
 }
 
-function _onVolumeChange() {
-  var v = VolumeStore.getVolume();
-  if (volume() !== v) {
-    setVolume(v);
-  }
-}
-
-function init() {
-  _audio.addEventListener("error", onPlayerEvent);
-  _audio.addEventListener("progress", onPlayerEvent);
-  _audio.addEventListener("play", onPlayerEvent);
-  _audio.addEventListener("pause", onPlayerEvent);
-  _audio.addEventListener("ended", onPlayerEvent);
-  _audio.addEventListener("timeupdate", onPlayerEvent);
-  _audio.addEventListener("loadedmetadata", onPlayerEvent);
-  _audio.addEventListener("loadstart", onPlayerEvent);
-
-  NowPlayingStore.addChangeListener(update);
-  NowPlayingStore.addControlListener(_onNowPlayingControl);
-  VolumeStore.addChangeListener(_onVolumeChange);
-
-  update();
-  _onVolumeChange();
-}
-
-var AudioAPI = {
-
-  init: function() {
-    init();
-  },
-
-};
-
-module.exports = AudioAPI;
+export default new AudioAPI();
