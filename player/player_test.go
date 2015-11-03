290b1d8baa1a114994b4e012b7997ae1d8dbf5c7
David Howden <dhowden@gmail.com>
2015-11-03 21:54:15 +1100

    Added test for Players

---
+++ player/player_test.go
@@ -0,0 +1,48 @@
+package player
+
+import "testing"
+
+type testPlayer string
+
+func (p testPlayer) Key() string           { return string(p) }
+func (testPlayer) Do(a Action) error       { return nil }
+func (testPlayer) SetMute(bool) error      { return nil }
+func (testPlayer) SetRepeat(bool) error    { return nil }
+func (testPlayer) SetVolume(float64) error { return nil }
+func (testPlayer) SetTime(float64) error   { return nil }
+
+func TestPlayers(t *testing.T) {
+	oneKey := "one"
+	onePl := testPlayer(oneKey)
+
+	ps := NewPlayers()
+
+	list := ps.List()
+	if len(list) != 0 {
+		t.Errorf("len(ps.List()) = %d, expected: %d", len(list), 0)
+	}
+
+	ps.Add(onePl)
+
+	p := ps.Get(oneKey)
+	if p != onePl {
+		t.Errorf("Get(%#v) = %#v, expected: %#v", oneKey, p, onePl)
+	}
+
+	list = ps.List()
+	if len(list) != 1 {
+		t.Errorf("len(ps.List()) = %d, expected: %d", len(list), 1)
+	}
+
+	ps.Remove(oneKey)
+
+	p = ps.Get(oneKey)
+	if p != nil {
+		t.Errorf("Get(%#v) = %#v, expected: %#v", oneKey, p, nil)
+	}
+
+	list = ps.List()
+	if len(list) != 0 {
+		t.Errorf("len(ps.List()) = %d, expected: %d", len(list), 0)
+	}
+}
