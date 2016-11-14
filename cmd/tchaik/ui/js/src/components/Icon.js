b4e5e1b1959748e269d98aef2e5e988fc5e433ff
David Howden <dhowden@gmail.com>
2016-11-14 19:01:34 +1100

    ui: Collapse Icon into pure component.

--- cmd/tchaik/ui/js/src/components/Icon.js
+++ cmd/tchaik/ui/js/src/components/Icon.js
@@ -4,31 +4,24 @@ import React from "react";
 
 import classNames from "classnames";
 
+const Icon = ({icon, extraClasses, ...others}) => {
+  var classes = {
+    icon: true,
+  };
+  classes["material-icons"] = true;
 
-export default class Icon extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return this.props.icon != nextProps.icon;
-  }
-
-  render() {
-    var {icon, extraClasses, ...others} = this.props;
-    var classes = {
-      icon: true,
-    };
-
-    classes["material-icons"] = true;
-
-    if (extraClasses) {
-      for (let k in extraClasses) {
-        classes[k] = extraClasses[k];
-      }
+  if (extraClasses) {
+    for (let k in extraClasses) {
+      classes[k] = extraClasses[k];
     }
-
-    return <span {...others} className={classNames(classes)}>{icon}</span>;
   }
+
+  return <span {...others} className={classNames(classes)}>{icon}</span>;
 }
 
 Icon.propTypes = {
   icon: React.PropTypes.string.isRequired,
   extraClasses: React.PropTypes.object,
 };
+
+export default Icon;
